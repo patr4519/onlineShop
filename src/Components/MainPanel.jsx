8384b52377cc7235d4a4ba8e881ba1d5d7e1a0f6
patr4519 <jero96@mail.ru>
2023-01-22 19:37:22 +0300

    +

--- src/Components/MainPanel.jsx
+++ src/Components/MainPanel.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Basket } from "./Basket";
-
+import searchIMG  from "../img/searchIMG.svg";
+ 
 export const MainPanel = ({setSearchValue, searchValue, basket, openBasket }) => {
     return (
         <div className="mainPanel">
             <div className="inputForm">
                 <h2>Products</h2>
+                <img src={searchIMG} alt='searchIMG' className="searchIMG"/>
                 <input 
                 className="inputSearch" 
                 value={searchValue} 
