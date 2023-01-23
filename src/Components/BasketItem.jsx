95f69bd8c2f0a8ae5589234c4d401be2195cea89
patr4519 <jero96@mail.ru>
2023-01-23 16:45:48 +0300

    +

--- src/Components/BasketItem.jsx
+++ src/Components/BasketItem.jsx
@@ -3,6 +3,19 @@ import React from "react";
 export const BasketItem = ({ item, openBasket }) => {
     const [counter, setCounter] = React.useState(1);
 
+    const getTotalPrice = () => {
+        let totalPrice = 0;
+        totalPrice = parseFloat(item.price) * counter;
+        if (totalPrice < 0) return 0;
+        return totalPrice;
+    }
+
+    const counterMinus = () => {
+        if (counter > 0) {
+            setCounter(counter - 1)
+        }
+    }
+
     return (
         <div className="basketItem">
             <div className="product">
@@ -21,7 +34,7 @@ export const BasketItem = ({ item, openBasket }) => {
                 <p className="basketTitles">COUNTERS</p>
                 <div className="centered">
                     <div className="counterWrapper">
-                        <div className="plusMinus" onClick={() => setCounter(counter - 1)}>-</div>
+                        <div className="plusMinus" onClick={counterMinus}>-</div>
                         <div className="counter">{counter}</div>
                         <div className="plusMinus" onClick={() => setCounter(counter + 1)}>+</div>
                     </div>
@@ -29,7 +42,8 @@ export const BasketItem = ({ item, openBasket }) => {
             </div>
             <div className="total">
                 <p className="basketTitles">TOTAL</p>
-                <div className="centered">Item total: {parseFloat(item.price)*counter}$</div>
+                {/* <div className="centered">Item total: {parseFloat(item.price)*counter}$</div> */}
+                <div className="centered">{`Item total: ${getTotalPrice()}$`}</div>
             </div>
         </div>
     );
