50b79a3c9bb095ffdc4361056948d814edd36306
patr4519 <jero96@mail.ru>
2023-01-23 15:06:36 +0300

    +

--- src/Components/BasketItem.jsx
+++ src/Components/BasketItem.jsx
@@ -1,7 +1,8 @@
 import React from "react";
-import closeIMG from "../img/closeIMG.svg";
 
 export const BasketItem = ({ item, openBasket }) => {
+    const [counter, setCounter] = React.useState(0);
+
     return (
         <div className="basketItem">
             <div className="product">
@@ -24,14 +25,6 @@ export const BasketItem = ({ item, openBasket }) => {
                 <p className="basketTitles">TOTAL</p>
                 <div className="centered">Item total: {item.price}</div>
             </div>
-            {/* <div className="closeWrapper" onClick={openBasket}>
-                <img className="closeIMG" src={closeIMG} alt='close' />
-            </div> */}
-            {/* <div className="outer" onClick={openBasket}>
-                <div className="inner">
-                    <label>Close</label>
-                </div>
-            </div> */}
         </div>
     );
 }
